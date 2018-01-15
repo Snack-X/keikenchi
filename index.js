a6cc962f0e18de693b2e58e79e3655451d517589
Snack <korsnack@korsnack.kr>
2018-01-15 22:55:11 +0900

    Add jpg/png support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const koa = require("koa");
+const canvas = require("./node_modules/canvas-prebuilt/canvas/index");
 
 const WEB_PORT = 20012;
 
@@ -35,7 +36,7 @@ async function requestHandler(ctx) {
     return;
   }
 
-  const matchImage = url.match(/^\/image\/([0-5]{47})\.(svg)$/);
+  const matchImage = url.match(/^\/image\/([0-5]{47})\.(svg|jpg|png)$/);
   if(method === "GET" && matchImage) {
     const data = matchImage[1], type = matchImage[2];
 
@@ -44,9 +45,27 @@ async function requestHandler(ctx) {
       ctx.body = renderAsSvg(ctx.imageTemplate, data);
       return;
     }
+    else if(type === "jpg" || type === "png") {
+      const cv = renderAsCanvas(ctx.imageTemplate, data);
+      let outputStream;
+
+      if(type === "jpg") {
+        ctx.type = "image/jpg";
+        outputStream = cv.jpegStream();
+      }
+      else if(type === "png") {
+        ctx.type = "image/png";
+        outputStream = cv.pngStream();
+      }
+
+      const buffers = [];
+      outputStream.on("data", function(chunk) { buffers.push(chunk); });
+      outputStream.on("end", function() { ctx.body = Buffer.concat(buffers); });
+    }
     else {
       ctx.status = 415;
       ctx.body = errorBody;
+      return;
     }
   }
 
@@ -65,4 +84,108 @@ function renderAsSvg(template, data) {
   }
 
   return output;
+}
+
+//==============================================================================
+
+function renderAsCanvas(template, data) {
+  const cv = canvas.createCanvas(1000, 1000);
+  const ctx = cv.getContext("2d");
+
+  // Background
+  ctx.lineWidth = 0;
+  ctx.fillStyle = "#dff";
+  ctx.rect(0, 0, 1000, 1000);
+  ctx.fill();
+
+  // Get paths
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#000";
+
+  const re = /<path class="prefecture __keiken_(\d+)__" data-name="(.+)" d="(.+)"\/>/g;
+  let match;
+
+  while((match = re.exec(template)) !== null) {
+    // Parse path data and draw
+    const index = parseInt(match[1], 10), d = match[3];
+
+    ctx.fillStyle = [ "#fff", "#0ff", "#0f0", "#ff0", "#f00", "#f0f" ][parseInt(data[index])];
+    drawPath(ctx, d);
+  }
+
+  // Draw other elements
+  match = template.match(/<path class="separator" d="(.+)"\/>/);
+  if(match) {
+    drawPath(ctx, match[1]);
+    ctx.stroke();
+  }
+
+  match = template.match(/<text fill="(.+)" text-anchor="end" font-size="(\d+)" x="(\d+)" y="(\d+)">(.+)<\/text>/);
+  if(match) {
+    ctx.fillStyle = match[1];
+    ctx.font = `${parseFloat(match[2]) * 0.75}pt sans-serif`;
+    const metrics = ctx.measureText(match[5]);
+
+    let targetX = parseFloat(match[3]), targetY = parseFloat(match[4]);
+    targetX = (targetX - metrics.width);
+
+    ctx.fillText(match[5], targetX, targetY);
+  }
+
+  // Finished
+  return cv;
+}
+
+function drawPath(ctx, d) {
+  const re = /([MLHVZ])(?:(\-?[0-9.]+),?(\-?[0-9.]+)?)?/gi;
+  let op, posX = 0, posY = 0;
+
+  while((op = re.exec(d)) !== null) {
+    switch(op[1]) {
+      case "M":
+        ctx.beginPath();
+        posX = parseFloat(op[2]);
+        posY = parseFloat(op[3]);
+        ctx.moveTo(posX, posY);
+        break;
+
+      case "L":
+        posX = parseFloat(op[2]);
+        posY = parseFloat(op[3]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "l":
+        posX += parseFloat(op[2]);
+        posY += parseFloat(op[3]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "H":
+        posX = parseFloat(op[2]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "h":
+        posX += parseFloat(op[2]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "V":
+        posY = parseFloat(op[2]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "v":
+        posY += parseFloat(op[2]);
+        ctx.lineTo(posX, posY);
+        break;
+
+      case "z": case "Z":
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        break;
+    }
+  }
 }
